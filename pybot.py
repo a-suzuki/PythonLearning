4062f777210ba3928f7cc3960c75ad7b1c78b0df
Atsushi Suzuki <atsushi.suzuki@tel.com>
2017-08-18 19:23:33 +0900

    :art: 平成メソッドを別ファイル化

--- pybot.py
+++ pybot.py
@@ -1,12 +1,4 @@
-def heisei_command(command):
-    heisei, year_str = command.split()
-    year = int(year_str)
-    if year >= 1989:
-        heisei_year = year - 1988
-        response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
-    else:
-        response = '西暦{}年ハ、平成デハアリマセン'.format(year)
-    return response
+from heisei import heisei_command
 
 with open('pybot.txt', encoding='utf-8') as open_file:
     raw_data = open_file.read()
