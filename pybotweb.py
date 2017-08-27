ace267b91f9bd4bee657513c7ea1772e0b31e9e4
Atsushi Suzuki <atsushisuzuki.mail@gmail.com>
2017-08-27 23:48:07 +0900

    :new: BootStrap対応

--- pybotweb.py
+++ pybotweb.py
@@ -1,5 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+
 from bottle import route, run, template, request
+from bottle import TEMPLATE_PATH
+from bottle import static_file
 from pybot import pybot
+ 
+# index.pyが設置されているディレクトリの絶対パスを取得
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# テンプレートファイルを設置するディレクトリのパスを指定
+TEMPLATE_PATH.append(BASE_DIR + "/views")
+
+@route('/css/<filename>')
+def css_dir(filename):
+    """ set css dir """
+    return static_file(filename, root=BASE_DIR+"/static/css")
+ 
+@route('/js/<filename>')
+def js_dir(filename):
+    """ set js dir """
+    return static_file(filename, root=BASE_DIR+"/static/js")
+ 
+@route('/img/<filename>')
+def img_dir(filename):
+    """ set img file """
+    return static_file(filename, root=BASE_DIR+"/static/img")
+ 
+@route('/font/<filename>')
+def font_dir(filename):
+    """ set font file """
+    return static_file(filename, root=BASE_DIR+"/static/fonts")
 
 @route('/hello')
 def hello():
