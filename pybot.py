9d45335339ae11262c700f10025763d6354f7a48
Atsushi Suzuki <atsushisuzuki.mail@gmail.com>
2017-08-27 22:39:48 +0900

    :new: add eto method

--- pybot.py
+++ pybot.py
@@ -1,4 +1,5 @@
 from heisei import heisei_command
+from eto import eto_command
 
 with open('pybot.txt', encoding='utf-8') as open_file:
     raw_data = open_file.read()
@@ -20,9 +21,12 @@ while True:
             break
     if '平成' in command:
         response = heisei_command(command)
+    if '干支' in command:
+        response = eto_command(command)
     if not response:
         response = '何ヲ言ッテイルカ、ワカリマセン'
     print(response)
+    return response
 
     if 'さようなら' in command:
         break
